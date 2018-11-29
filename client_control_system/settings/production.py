d68549963e46514686783b51b74bf7e489b5b121
Aleh Zaksheuski <aleh.zaksheuski@leverx.com>
2018-11-29 14:52:24 +0300

    #master Add new database settings

--- client_control_system/settings/production.py
+++ client_control_system/settings/production.py
@@ -4,10 +4,13 @@ from client_control_system.settings.base import *
 
 DEBUG = False
 
-django_heroku.settings(locals())
 DATABASES['default'] = dj_database_url.config()
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+django_heroku.settings(locals())
+
+
