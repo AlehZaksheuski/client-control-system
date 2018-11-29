cf9b2cf997ba4b0cedfe6d4c272cfa7d62ecb1f0
Aleh Zaksheuski <aleh.zaksheuski@leverx.com>
2018-11-29 13:36:02 +0300

    #master. Add django heroku setup

--- client_control_system/settings.py
+++ client_control_system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,3 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
