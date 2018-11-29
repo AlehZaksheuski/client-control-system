a2b91b437e1a9f1986efe9340358a1e42dbb9139
Aleh Zaksheuski <aleh.zaksheuski@leverx.com>
2018-11-29 14:16:25 +0300

    #master. Add gitingone, move settings

---
+++ client_control_system/settings/local.py
@@ -0,0 +1,1 @@
+from client_control_system.settings import *
--- client_control_system/wsgi.py
+++ client_control_system/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'client_control_system.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'client_control_system.settings.production')
 
 application = get_wsgi_application()
