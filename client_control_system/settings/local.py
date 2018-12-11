707d7b711425c9beb7400ee5be536e00642ffd20
Aleh Zaksheuski <aleh.zaksheuski@leverx.com>
2018-12-11 19:41:19 +0300

    #master. Add new local settings for database sqlite

--- client_control_system/settings/local.py
+++ client_control_system/settings/local.py
@@ -1,1 +1,13 @@
 from client_control_system.settings.base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '../../BackupData/sqlite3.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {'charset': 'utf8mb4'},
+    }
+}
